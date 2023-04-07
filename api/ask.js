a64f670004d27899eb17236f6307fcc2feb89006
César Guadarrama <cesargdm@icloud.com>
2023-04-07 13:49:45 -0600

    Update ask

--- api/ask.js
+++ api/ask.js
@@ -7,15 +7,16 @@ const configuration = new Configuration({
 const MODEL = 'davinci:ft-personal-2023-04-07-19-02-24'
 
 module.exports = async function (request, response) {
-	const { prompt = "What's your name?" } = request.query
+	const { prompt: rawPrompt = "What's your name?" } = request.query
 
 	const openai = new OpenAIApi(configuration)
 
 	const result = await openai.createCompletion({
+		prompt: `${rawPrompt.slice(0, 100)}->`,
 		model: MODEL,
-		prompt,
 		max_tokens: 20,
 		temperature: 0,
+		stop: ['END'],
 	})
 
 	response.setHeader('Cache-Control', 's-maxage=86400')
