7ef2f3dccb450853056c1e84c7286ed8596d60da
César Guadarrama <cesargdm@icloud.com>
2023-04-07 13:21:55 -0600

    add missing module

--- api/ask.ts
+++ api/ask.ts
@@ -1,4 +1,5 @@
-const { Configuration, OpenAIApi } = require('openai')
+import { Configuration, OpenAIApi } from 'openai'
+
 const configuration = new Configuration({
 	apiKey: process.env.OPENAI_API_KEY,
 })
