456ccaacce512342c7d595afbd686c4136e182b1
César Guadarrama <cesargdm@icloud.com>
2023-05-02 21:34:18 -0600

    Update

---
+++ src/app/api/twitter/last-tweet/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+	const userId = '313716452'
+
+	const data = await fetch(
+		`https://api.twitter.com/2/users/${userId}/tweets?max_results=5&exclude=retweets,replies&expansions=attachments.media_keys&media.fields=url,type,public_metrics&tweet.fields=attachments,created_at,geo,public_metrics,source&place.fields=country`,
+		{
+			headers: {
+				Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}`,
+			},
+		},
+	).then((response) => response.json())
+
+	data.data = data.data[0]
+
+	return NextResponse.json(data)
+}
