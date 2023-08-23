6eefc423b2d1a0bf9a4e9937c7c32f288e711632
César Guadarrama <cesargdm@icloud.com>
2023-08-23 11:21:57 -0600

    Upgrade dependencies

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -2,12 +2,13 @@ import { NextResponse } from 'next/server'
 import { sql } from '@vercel/postgres'
 import { Configuration, OpenAIApi } from 'openai'
 
-const configuration = new Configuration({
-	apiKey: process.env.OPENAI_API_KEY,
-})
+const openAiApiKey = process.env.OPENAI_API_KEY
+
+const configuration = new Configuration({ apiKey: openAiApiKey })
+
 const openai = new OpenAIApi(configuration)
 
-const MODEL = 'davinci:ft-personal-2023-04-07-20-32-03'
+const MODEL_ID = 'davinci:ft-personal-2023-04-07-20-32-03'
 
 export const revalidate = 60 * 60 * 6
 
@@ -17,33 +18,30 @@ export async function GET(request: Request) {
 	const prompt = `${rawPrompt.trim().replace('->', '').slice(0, 50)}->`
 
 	if (!prompt) {
-		return new Response('Missing prompt', {
-			status: 400,
-		})
+		return new Response('Missing prompt', { status: 400 })
 	}
+
+	const date = new Date().toISOString()
 
 	try {
 		const { data } = await openai.createCompletion({
 			prompt: prompt,
-			model: MODEL,
+			model: MODEL_ID,
 			stop: ['END'],
 			max_tokens: 50,
 			temperature: 0.8,
 		})
 
-		const completion = data.choices[0].text
+		const { text: completion } = data.choices[0] ?? {}
 
-		// Save prompt to database
 		await sql`
-			INSERT INTO prompts (prompt, completion) VALUES (${prompt}, ${completion ?? ''})
+			INSERT INTO prompts (prompt, completion, date) VALUES (${prompt}, ${completion}, ${date})
 		`.catch(() => undefined)
 
 		return NextResponse.json(data)
 	} catch (error) {
 		console.error(error)
 
-		return new Response('Server error', {
-			status: 500,
-		})
+		return new Response('Server error', { status: 500 })
 	}
 }
