035e878dbcfb421e5ccda4d6a234d7a0d4fa580a
César Guadarrama <cesargdm@icloud.com>
2023-10-02 18:52:06 -0600

    Add workaround for musk ideas

---
+++ src/app/api/x/me/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from 'next/server'
+import OAuth from 'oauth'
+
+/**
+ * 3 days
+ */
+export const revalidate = 60 * 60 * 24 * 3
+
+const oauth = new OAuth.OAuth(
+	'https://api.twitter.com/oauth/request_token',
+	'https://api.twitter.com/oauth/access_token',
+	process.env.X_API_KEY as string,
+	process.env.X_API_KEY_SECRET as string,
+	'1.0A',
+	null,
+	'HMAC-SHA1',
+)
+
+export async function GET() {
+	const data = await new Promise((resolve, reject) =>
+		oauth.get(
+			'https://api.twitter.com/2/users/me?user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,url,username,verified,withheld&expansions=pinned_tweet_id',
+			process.env.X_ACCESS_TOKEN as string,
+			process.env.X_ACCESS_TOKEN_SECRET as string,
+			(error, data, response) => {
+				console.log({ data, response })
+				if (error) return reject(error)
+
+				return resolve(JSON.parse(data as string))
+			},
+		),
+	)
+
+	return NextResponse.json(data, {
+		headers: {
+			'Content-Type': 'application/json; charset=utf-8',
+		},
+	})
+}
--- src/modules/LastTweet/index.tsx
+++ src/modules/LastTweet/index.tsx
@@ -3,7 +3,7 @@ import X from '@/assets/icons/X'
 import { readTweetsButton, tweetParagraph } from './styles.css'
 
 function getData() {
-	return fetch('https://cesargdm.com/api/x/last-posts', {
+	return fetch('https://cesargdm.com/api/x/me', {
 		next: { revalidate: 60 * 60 * 24 },
 	})
 		.then((response) => response.json())
@@ -13,24 +13,53 @@ function getData() {
 async function LastTweet() {
 	const result = await getData()
 
-	const tweetText = result?.data?.[0]?.text
+	const tweetText = result?.includes?.tweets.text
+	const description = result?.data.description
+
+	const readMore = (
+		<a
+			target="_blank"
+			rel="noopener noreferrer"
+			className={readTweetsButton}
+			href="https://twitter.com/cesargdm"
+			aria-label="Visit my Twitter profile"
+		>
+			Read more posts
+		</a>
+	)
+
+	if (tweetText) {
+		return (
+			<>
+				<h2>
+					<X style={{ width: 24, height: 24 }} aria-hidden />
+					Posts
+				</h2>
+				<p className={tweetParagraph}>{tweetText}</p>
+				{readMore}
+			</>
+		)
+	}
+
+	if (description) {
+		return (
+			<>
+				<h2>
+					<X style={{ width: 24, height: 24 }} aria-hidden />
+				</h2>
+				<p className={tweetParagraph}>{result.data.description}</p>
+				{readMore}
+			</>
+		)
+	}
 
 	return (
 		<>
 			<h2>
 				<X style={{ width: 24, height: 24 }} aria-hidden />
-				Posts
 			</h2>
-			<p className={tweetParagraph}>{tweetText}</p>
-			<a
-				target="_blank"
-				rel="noopener noreferrer"
-				className={readTweetsButton}
-				href="https://twitter.com/cesargdm"
-				aria-label="Visit my Twitter profile"
-			>
-				Read more posts
-			</a>
+			<p className={tweetParagraph}>@cesargdm</p>
+			{readMore}
 		</>
 	)
 }
